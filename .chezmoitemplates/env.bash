822744e81e09371edcb2550b88225b70de73b03b
Souvenal <2206794044@qq.com>
2026-03-07 04:56:34 +0800

    feat: modified to mac config

--- .chezmoitemplates/env.bash
+++ .chezmoitemplates/env.bash
@@ -1,25 +1,43 @@
-# User SDK Root
-export DEV_SDK_ROOT="{{ .user.dev_sdk_root }}"
-
-# XDG Config
+# XDG config
 export XDG_CONFIG_HOME="{{ .user.xdg_config_home }}"
+
+# Network proxy
+export HTTP_PROXY="{{ .proxy.http }}"
+export HTTPS_PROXY="{{ .proxy.https }}"
+
+# Dev config
+export DEV_SDK_ROOT="{{ .lib.dev_sdk_root }}"
+export PKG_CONFIG_PATH="{{ .lib.pkg_config_path }}:$PKG_CONFIG_PATH"
+
+# C/C++ settings
+export CC="{{ .language.cpp.cc }}"
+export CXX="{{ .language.cpp.cxx }}"
+export CMAKE_GENERATOR="{{ .language.cpp.cmake_generator }}"
+export CMAKE_BUILD_PARALLEL_LEVEL=
+{{- if eq .chezmoi.os "darwin" }}
+$(sysctl -n hw.ncpu)
+{{- else if eq .chezmoi.os "linux" }}
+$(nproc)
+{{- else }}
+8
+{{ end }}
 
 # Vulkan SDK
 {{ if eq .tools.vulkan.version "" -}}
 # WARNING: Vulkan SDK not configured
 {{ else -}}
-export VULKAN_SDK_VERSION="{{ .tools.vulkan.version }}"
+export VULKAN_SDK_VERSION="{{ .lib.vulkan_version }}"
 export VULKAN_SDK_ROOT="$DEV_SDK_ROOT/VulkanSDK/$VULKAN_SDK_VERSION"
 if [ -f "$VULKAN_SDK_ROOT/setup-env.sh" ]; then
     source "$VULKAN_SDK_ROOT/setup-env.sh"
 fi
-{{ end -}}
+{{ end }}
 
-# CUDA
-{{ if eq .tools.cuda.version "" -}}
-# WARNING: CUDA not configured
-{{ else -}}
-export CUDA_VERSION="{{ .tools.cuda.version }}"
-export CUDA_ROOT="/usr/local/cuda-{{ .tools.cuda.version }}"
-export PATH="$CUDA_ROOT/bin:$PATH"
-{{ end -}}
+# Homebrew settings
+BREW=$(which brew 2>/dev/null)
+if [ -x "$BREW" ]; then
+    export HOMEBREW_PIP_INDEX_URL="{{ .app.homebrew.pip_index_url }}"
+    export HOMEBREW_API_DOMAIN="{{ .app.homebrew.api_domain }}"
+    export HOMEBREW_BOTTLE_DOMAIN="{{ .app.homebrew.bottle_domain }}"
+    eval "$($BREW shellenv)"
+fi
