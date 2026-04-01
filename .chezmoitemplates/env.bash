b5ae031d9f03eb063bf457fe038bcaf0312a27c0
Souvenal <souvenal777@gmail.com>
2026-04-02 02:13:06 +0800

    chore: add some xdg dirs

--- .chezmoitemplates/env.bash
+++ .chezmoitemplates/env.bash
@@ -5,9 +5,18 @@ export XDG_CONFIG_HOME={{ .system.xdg.config_home | quote }}
 export XDG_DATA_HOME={{ .system.xdg.data_home | quote }}
 export XDG_STATE_HOME={{ .system.xdg.state_home | quote }}
 
+# bash
+export HISTFILE="$XDG_STATE_HOME/bash/history"
+
 # Docker
 export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
 export MACHINE_STORAGE_PATH="$XDG_DATA_HOME/docker-machine"
+
+# dotnet
+export DOTNET_CLI_HOME="$XDG_DATA_HOME/dotnet"
+
+# go
+export GOPATH="$XDG_DATA_HOME/go"
 
 # GPG
 export GNUPGHOME="$XDG_DATA_HOME/gnupg"
